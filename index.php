84d8d318168062266775135170cf74b8ce544261
jayrajsinh solanki <jayrajsinhsolanki8008@gmail.com>
2022-02-28 12:27:45 +0530

    Hack removed initially..

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Dynamic Dependent Select Box</title>
+  <title><?php echo "Dynamic Dependent Select Box"; ?></title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
